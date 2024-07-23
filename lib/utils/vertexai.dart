34c9575b9705f9c01db5cfaabf4b3b71a7f22fe4
m0m0i <hirocorp01@gmail.com>
2024-07-23 16:01:34 +0900

    added prompt edit method and the debug comments

--- lib/utils/vertexai.dart
+++ lib/utils/vertexai.dart
@@ -2,6 +2,8 @@ import "dart:async";
 import "dart:convert";
 import "dart:typed_data";
 
+import "package:googleapis_auth/auth_io.dart";
+
 import "package:flutter/material.dart";
 import "package:google_next_imagen_demo/models/gemini_response.dart";
 import "package:google_next_imagen_demo/models/imagen_resopnse.dart";
@@ -17,10 +19,12 @@ class VertexAI {
       "Content-Type": "application/json; charset=utf-8"
     };
 
+    String updatedPrompt = await updatePrompt(prompt, credentials);
+
     var body = {
       "instances": [
         {
-          "prompt": prompt,
+          "prompt": updatedPrompt,
         }
       ],
       "parameters": {
@@ -32,7 +36,7 @@ class VertexAI {
       }
     };
 
-    debugPrint('...calling Imagen 3 API');
+    debugPrint('...calling Imagen 3 preview API');
     try {
       final res = await http.post(
         Uri.https(
@@ -46,15 +50,15 @@ class VertexAI {
         ImagenResponse imagenResponse =
             ImagenResponse.fromJson(jsonDecode(res.body));
 
-        debugPrint('Succeeded to generate iamges');
+        debugPrint('Succeeded to generate the iamge');
         return imagenResponse;
       } else {
         debugPrint('Error: ${res.statusCode} - ${res.body}');
-        throw Exception('Failed to edit image');
+        throw Exception('Failed to generate the image');
       }
     } catch (e) {
       debugPrint('Error editing image: $e');
-      throw Exception('Failed to edit image');
+      throw Exception('Failed to generate the image');
     }
   }
 
@@ -66,10 +70,12 @@ class VertexAI {
       "Content-Type": "application/json; charset=utf-8"
     };
 
+    String updatedPrompt = await updatePrompt(prompt, credentials);
+
     var body = {
       "instances": [
         {
-          "prompt": prompt,
+          "prompt": updatedPrompt,
           "image": {
             "bytesBase64Encoded": base64Encode(image),
           }
@@ -138,7 +144,7 @@ class VertexAI {
       }
     };
 
-    debugPrint('...calling Gemini API');
+    debugPrint('...calling Gemini 1.5 Flash to get the copy');
 
     try {
       final res = await http.post(
@@ -157,10 +163,64 @@ class VertexAI {
         return geminiResponse;
       }
     } catch (e) {
-      debugPrint('Failed to call Gemini API');
+      debugPrint('Failed to call Gemini 1.5 Flash');
       throw Exception(e.toString());
     }
 
     throw Exception('Failed to generate text');
+  }
+
+  FutureOr<String> updatePrompt(
+      String prompt, AccessCredentials credentials) async {
+    Map<String, String> headers = {
+      "Authorization": "Bearer ${credentials.accessToken.data}",
+      "Content-Type": "application/json; charset=utf-8"
+    };
+
+    var body = {
+      "contents": {
+        "role": "USER",
+        "parts": [
+          {
+            "text": '''
+              You are the excellent prompt generator to get the best results from Google Cloud Vertex Imagen API.
+              Your mission is to translate the Japanese to English and create the best prompt to get the most fitted images to the original prompt.
+
+              If input is English, you need to enhance it to get better images.
+
+              The generated prompt must be optimized to generate realistic, photographic and impactful images.
+              Also, please respond only generated English prompt.
+
+              input: $prompt
+            ''',
+          }
+        ]
+      }
+    };
+
+    debugPrint('...calling Gemini 1.5 Flash to enhance the prompt');
+
+    try {
+      final res = await http.post(
+        Uri.https(
+          'us-central1-aiplatform.googleapis.com',
+          'v1/projects/next-tokyo-imagen-flutter-demo/locations/us-central1/publishers/google/models/gemini-1.5-flash-001:generateContent',
+        ),
+        headers: headers,
+        body: jsonEncode(body),
+      );
+      if (res.statusCode == 200) {
+        GeminiResponse geminiResponse =
+            GeminiResponse.fromJson(jsonDecode(res.body));
+
+        debugPrint('Succeeded to generate the better prompt');
+        return geminiResponse.candidates[0].content.parts[0].text;
+      }
+    } catch (e) {
+      debugPrint('Failed to call Gemini 1.5 Flash');
+      throw Exception(e.toString());
+    }
+
+    throw Exception('Failed to generate better prompt');
   }
 }
